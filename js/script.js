d1484182f90f2a1f0819cab63d86a41e0fed5cd0
James Robinson <jamesr@blackmagicdesign.com>
2017-06-29 18:56:20 +1000

    slidey carousel

--- js/script.js
+++ js/script.js
@@ -1,29 +1,55 @@
 var slideIndex = 1;
+var clicked = false;
 showDivs(slideIndex);
 
 function plusDivs(n) {
+    clicked = true;
+    $($('.carousel-slides')[slideIndex-1]).css({'left': 0}).animate({
+        'left': '-100vw'
+    })
     showDivs(slideIndex += n);
 }
 
 function currentDiv(n) {
+    clicked = true;
     showDivs(slideIndex = n);
 }
 
 function showDivs(n) {
     var i;
-    var x = document.getElementsByClassName("carousel-slides");
-    if(x.length > 0) {
-        var dots = document.getElementsByClassName("carousel-button");
-        if (n > x.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = x.length}
-        for (i = 0; i < x.length; i++) {
-           x[i].style.display = "none";
+    var slide = $('.carousel-slides');
+    if(slide.length > 0) {
+        var dots = $('.carousel-button');
+        if (n > slide.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slide.length
+        }
+        if(!clicked) {
+            for (i = 0; i < slide.length; i++) {
+                $(slide[i]).css({'display': 'none'});
+            }
         }
         for (i = 0; i < dots.length; i++) {
            dots[i].className = dots[i].className.replace(" w3-white", "");
         }
-        x[slideIndex-1].style.display = "block";
+        $(slide[slideIndex-1]).show();
+        if(clicked) {
+            $(slide[slideIndex-1]).css({'position': 'absolute', 'z-index': 2, 'top': 0, 'left': '100vw'}).animate({
+                'left': 0
+            }, function(){
+                $(slide[slideIndex-1]).css({'position': 'relative', 'z-index': 1});
+                for (i = 0; i < slide.length; i++) {
+                    if(i == (slideIndex - 1)) {
+                        continue;
+                    }
+                    $(slide[i]).css({'display': 'none'});
+                }
+            })
+        }
         dots[slideIndex-1].className += " w3-white";
+        clicked = false;
     }
 }
 
@@ -49,5 +75,7 @@ function showDivs(n) {
             })
         })
     })
+
+    $('.services-banner')
 
 })();
